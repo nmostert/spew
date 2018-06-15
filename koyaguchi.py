c7801c038390df9fe4ccc3dec5729d3cd5d54266
Mostert <nmostert@massey.ac.nz>
2018-06-15 17:02:36 +1200

    geopandas added to requirements.txt

--- koyaguchi.py
+++ koyaguchi.py
@@ -1,7 +1,7 @@
 from tephra2io import tephra2_to_df
-import visualisation as vis
+# import visualisation as vis
 import sampling as smp
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
@@ -11,7 +11,11 @@ if __name__ == "__main__":
     x_0, y_0 = 532290, 1382690
     x_p, y_p = smp.find_nearest_grid_point(df, (x_0, y_0))
 
-    vis.plot_grid(df)
-    plt.plot(x_0, y_0, 'r*')
-    plt.plot(x_p, y_p, 'b*')
-    plt.show()
+    xx = list(set(df['Easting']))
+    xx.sort()
+
+    r_x = [i for i in xx if i >= x_p]
+    r_y = [y_p] * len(r_x)
+
+    df1 = df.loc[lambda df: df.Easting in r_x]
+    print(df1)
